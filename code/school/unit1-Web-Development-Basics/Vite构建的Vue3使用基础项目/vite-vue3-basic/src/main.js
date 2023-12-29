20933b53a6f0c82c94904642b66abc794cc9e65a
zhengyiyi <zrh182278@outlook.com>
2023-12-29 10:58:51 +0800

    Update main.js

--- code/school/unit1-Web-Development-Basics/Vite构建的Vue3使用基础项目/vite-vue3-basic/src/main.js
+++ code/school/unit1-Web-Development-Basics/Vite构建的Vue3使用基础项目/vite-vue3-basic/src/main.js
@@ -1,16 +1,16 @@
-import  {createApp}  from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router"  //import router from "./router/index.js"
 
 //在Vue 3项目里使用百度地图，需要先安装模块依赖  npm install vue-baidu-map-3x
 import BaiduMap from 'vue-baidu-map-3x'
 
-const app=createApp(App)
+const app = createApp(App)
 
 //BaiduMap是显示地图的容器组件，可以在其内设置地图的配置项和样式。
-app.use(BaiduMap,{
-    ak:'D87cvLuQn3pLo7vBxuZFGNLeiLdPgEf6'  //注册百度应用的key，适合于Vue 3项目。
-    //ak:'gn0hyTafn4d2f5nXtuP2Xm87QnY1abxr' //适合于Vue 2项目的key。使用它，搜索功能将失效。
+app.use(BaiduMap, {
+    ak: ''  //注册百度应用的key，适合于Vue 3项目。
+    //ak:'' //适合于Vue 2项目的key。使用它，搜索功能将失效。
 })
 
 app.use(router)    //使用路由是进行路由链接的前提
